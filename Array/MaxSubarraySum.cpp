b0b2b382627474b2ee6a42073d08814ce34de632
Mohd Jamil <mohammedjameel2412@gmail.com>
2022-10-17 12:53:51 +0000

    done

--- Array/MaxSubarraySum.cpp
+++ Array/MaxSubarraySum.cpp
@@ -7,7 +7,7 @@ int maxSubarraySum(int arr[], int n)
     int res = 0;
     int maxEnding = arr[0];
 
-    for(int i =0; i < n; i++)
+    for(int i = 0; i < n; i++)
     {
         maxEnding = max(maxEnding + arr[i],arr[i]);
         res = max(res, maxEnding);
