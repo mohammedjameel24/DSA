9d885af8e5deca6f2894700c0fa2f80008ff6943
Mohd Jamil <mohammedjameel2412@gmail.com>
2022-10-17 04:21:03 +0000

    done

--- Array/Leaders.cpp
+++ Array/Leaders.cpp
@@ -2,8 +2,22 @@
 
 using namespace std;
 
+void leaders(int arr[], int n)
+{
+    int current_leader = arr[n-1];
+    cout << current_leader << " ";
+    for(int i = n-2; i >= 0; i--)
+    {
+        if(current_leader < arr[i]){
+            current_leader = arr[i];
+            cout << current_leader << " ";
+        }
+    }
+}
+
 int main()
 {
-    int arr[]={10,5,4,3,2,1};
-    int n = sizeof(arr)/sizeof(arr[0];)
+    int arr[]={7,10,4,10,6,5,2};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    leaders(arr,n);
 }
