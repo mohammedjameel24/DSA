5c94a83dd81c78fdd802eceb277de8a00362281d
Mohd Jamil <mohammedjameel2412@gmail.com>
2022-10-21 15:00:59 +0000

    done

--- Array/FlipsToMakeSame.cpp
+++ Array/FlipsToMakeSame.cpp
@@ -2,50 +2,18 @@
 
 using namespace std;
 
-// pair<int,int> FlipsToMakeSame(int arr[] , int n)
-// {
-//     int Xcount = 0;
-//     int Ycount = 0;
-
-//     for(int i = 0; i < n ; i++)
-//     {
-//         switch(arr[i])
-//         {
-//             case 1:
-//                 Xcount++;
-//                 break;
-//             case 0:
-//                 Ycount++;
-//                 break;
-//         }
-
-//     }
-
-//     int flip = min(Xcount,Ycount);
-//     for(int i = 1; i<n ; i++)
-//     {
-//         if(arr[i] != arr[0])
-//         {
-
-//         }
-//     }
-
-//     return {Xcount,Ycount} ;
-
-// }
-
 void FlipsToMakeSame(int arr[], int n)
 {
     for(int i = 1; i < n; i++)
     {
-        if(arr[i] != arr[i-1])
+        if(arr[i] != arr[i-1]) // i = 1 : do nothing // i = 5 : do nothing // i = 7 : do nothing
         {
-            if(arr[i] != arr[0])
+            if(arr[i] != arr[0]) // i = 2 : print(i=2) // i = 3 : do nothing // i = 6 : print(i=6)
             {
                 cout << "from" << i << endl;
             }
             else{
-                cout << (i-1) << endl;
+                cout << (i-1) << endl; // i = 4 : print(i - 1 = 3) // i = 8 : print(i - 1 = 7)
             }
         }
     }
@@ -57,7 +25,7 @@ void FlipsToMakeSame(int arr[], int n)
 
 int main()
 {
-    int arr[] = {1,0,0,0,1,1,0,0,1,1,1,1};
+    int arr[] = {0,0,1,1,0,0,1,1,0};
     int n = sizeof(arr)/sizeof(arr[0]);
 
     FlipsToMakeSame(arr,n);
