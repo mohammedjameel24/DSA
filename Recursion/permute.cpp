336cd07a908aa97d999adf409c2a0c946ecb7507
Mohd Jamil <mohammedjameel2412@gmail.com>
2022-11-02 16:49:20 +0000

    done

--- Recursion/permute.cpp
+++ Recursion/permute.cpp
@@ -9,13 +9,13 @@ void permute(string s, int i = 0)
     {
         cout << s << " ";
         return;
-    }
+    } // return empty string if length = 0
     for(int j = i; j < s.length();j++)
     {
         swap(s[i],s[j]);
         permute(s, i+1);
         swap(s[i],s[j]);
-    }
+    } // swap [i] with [j] and increment i on every permute 
 }
 int main()
 {
