a53205bf6bee107c82e5ee0149391e8209e233dc
unknown <mohdjameel08@gmail.com>
2023-03-08 10:35:27 +0530

    back

--- Recursion/subSets.cpp
+++ Recursion/subSets.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string.h>
+#include <vector>
 using namespace std;
 
 
@@ -15,10 +16,34 @@ void subSets(string str, int index = 0, string curr = "")
 	subSets(str, index + 1, curr + str[index]);
 	subSets(str, index + 1, curr);
 }
+int countUniqueChars(string s) {
+    vector<int> freq(26, 0);
+    int count = 0;
+    for (char c : s) {
+        if (freq[c - 'A'] == 0) {
+            count++;
+        }
+        freq[c - 'A']++;
+    }
+    return count;
+}
+
+int countSubstringUniqueChars(string s) {
+    int n = s.length();
+    int res = 0;
+    for (int k = 1; k <= n; k++) {
+        for (int i = 0; i <= n - k; i++) {
+            string t = s.substr(i, k);
+            res += countUniqueChars(t);
+        }
+    }
+    return res;
+}
 
 int main()
 {
-	string str = "abc";
-	subSets(str);
+	string str = "ABA";
+	//subSets(str);
+	cout << countSubstringUniqueChars(str);
 	return 0;
 }
